ca4e0ec4e7e51f57262fb27abfeb18e31d1f3ade
Alex_villada <92128223+Ale-pool@users.noreply.github.com>
2025-10-28 20:38:24 -0500

    seccion 9 - 28-10-25

---
+++ funciones_integradoras.py
@@ -0,0 +1,30 @@
+
+# funciones integradoras
+
+num_1 = 19
+num_2 = 23.29
+
+c = complex(num_1, num_2) # crear un número complejo
+print(c)
+print(type(c))
+
+cadena = "Hola, ¿qué tal?" + str(42) # concatenar cadena y número y usamos la función str() para convertir el número en una cadena
+print(cadena)
+
+
+abs_val = abs(-56)  # valor absoluto
+print(abs_val)
+
+round_val = round(3.14659, 2)  # redondear a 2 decimales
+print(round_val)
+
+
+n = 10
+eval_val = eval('n + 5')  # evaluar una expresión como código Python
+print(eval_val)
+
+length = len(cadena)  # longitud de la cadena
+print(length)
+
+
+help()  # mostrar la documentación de la función len
